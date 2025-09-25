25d1a38c4c64df1cfc6d8d9377922008ed7994c9
flynth_dev <flynth.dev@gmail.com>
2025-09-24 21:30:48 -0400

    Update sitemap for dns .bo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://liriun-ux.vercel.app', // tu dominio
+  siteUrl: 'https://www.liriun-ux.tecnologia.bo', // tu dominio
   generateRobotsTxt: true, // genera robots.txt automáticamente
   sitemapSize: 5000, // máximo de URLs por sitemap
 };
